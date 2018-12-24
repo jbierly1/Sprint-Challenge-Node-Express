7f1086cdd667744c2de1b541d6d1673e57cde3fe
jbierly1 <james.bierly@gmail.com>
2018-12-24 09:46:50 -0500

    built post method for projects

--- api/server.js
+++ api/server.js
@@ -24,3 +24,31 @@ server.get('/api/projects', (req, res) => {
             .json({ error: 'The projects information could not be retrieved.' });
     });
 })
+
+server.get('/api/projects/:id', (req, res) =>{
+    let id=req.params.id;
+    dbProject.get(id)
+    .then(project=>{
+        res.status(200).json(project)
+    }).catch(err=>{res.status(500).json({error:'The project info is not found'})})
+})
+
+server.post('/api/projects', (req, res)=>{
+    let newproj=req.body;
+
+    if(newproj.name,newproj.description){
+
+        if(newproj.name.length<=128){
+    dbProject.insert(newproj)
+    .then(
+        project=>{
+            res.status(200).json(project);
+        }
+    ).catch(err=>{res.status(500).json({error:"could not save project"})})
+
+}else{res.status(500).json({error:"name must be less than 128 characters!"})}
+}
+    else{
+        res.status(400).json({error:"please provide name and description"})
+    }
+})
